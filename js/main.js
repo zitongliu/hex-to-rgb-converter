eab1a34fe27c53750f8cdd45dddf904f97c8bdba
zitongliu <zitongliu7@gmail.com>
2017-01-11 22:47:39 +1100

    Fixed hex to rgb conversion

--- js/main.js
+++ js/main.js
@@ -21,38 +21,43 @@ var hexToDecimalObject = {
   "f":"15"
 };
 
+// ====== Function that converts a hex input into decimal output ====== //
 var hexToDecimal = function(hexIn){
   var hexArray = hexIn.split('');
   var decimalString = "";
   hexArray.map(function(element){
     decimalString += hexToDecimalObject[element];
   });
-  return decimalString;
+  var decimalInteger = parseInt(decimalString);
+  return decimalInteger;
 };
 
-console.log(hexToDecimal('bb3399'));
-
-
-var hexToRgba = function(input){
+// ====== Function that converts a hex color into rgb color ====== //
+// ====== Output is a string in format: rgb(xxx,xxx,xxx) ====== //
+var hexToRgb = function(input){
   var hexArray = input.split("");
   var resultsArray = [];
 
   for (var i = 0; i<hexArray.length; i+=2){
-    resultsArray.push( hexToDecimal(hexArray[i] + hexArray[i+1]) );
+    resultsArray.push( hexToDecimal(hexArray[i])*16 + hexToDecimal(hexArray[i+1]) );
   }
 
   var rgbString = "rgb("+resultsArray[0]+","+resultsArray[1]+","+resultsArray[2]+")";
-  console.log(rgbString);
   return rgbString;
 };
 
-var test = hexToRgba("663399");
 
 $(document).ready(function(){
 
+  // ====== Add event listener to hex input tag. Listen for keyup ======
   $("#hex").on("keyup",function(){
+    // ====== If the length of hex input is 6, run the hexToRgb function ======
     if( this.value.length === 6){
-      $("html").css("backgroundColor", hexToRgba(this.value));
+      $("html").css("backgroundColor", "#"+this.value);
+      var rgbValues = hexToRgb(this.value);
+
+      // ====== Set the rgb equivalent value inside rgb input box ======
+      $("#rgb").val(rgbValues);
     }
 
   });
